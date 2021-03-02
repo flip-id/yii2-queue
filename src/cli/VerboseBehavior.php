c08b8712b6f899f149eb70016c620c9ede4e0b46
indrabayu.rizal <indrabayu.rizal@gmail.com>
2021-03-03 00:08:41 +0700

    add after exec error event

--- src/cli/VerboseBehavior.php
+++ src/cli/VerboseBehavior.php
@@ -105,6 +105,7 @@ class VerboseBehavior extends Behavior
         $this->command->stdout($this->jobTitle($event), Console::FG_GREY);
         $this->command->stdout(' - ', Console::FG_YELLOW);
         $this->command->stdout('Error', Console::BG_RED);
+        $duration = 0;
         if ($this->jobStartedAt) {
             $duration = number_format(round(microtime(true) - $this->jobStartedAt, 3), 3);
             $this->command->stdout(" ($duration s)", Console::FG_YELLOW);
@@ -114,6 +115,17 @@ class VerboseBehavior extends Behavior
         $message = explode("\n", ltrim($event->error->getMessage()), 2)[0]; // First line
         $this->command->stdout($message, Console::FG_GREY);
         $this->command->stdout(PHP_EOL);
+
+        \Yii::$app->trigger(self::EVENT_AFTER_EXEC_VERBOSE, (
+            new ExecEvent([
+                'id' => $event->id,
+                'job' => $event->job,
+                'ttr' => $event->ttr,
+                'attempt' => $event->attempt,
+                'error' => $event->error,
+                'duration' => $duration
+            ])
+        ));
     }
 
     /**
